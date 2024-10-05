ecdd679256e3beb3d90efa9748364ba1e9e00c82
Ben Bonus <jbenbonus@gmail.com>
2024-10-04 20:43:45 -0700

    Connected to a mysql database. Also did some site stying

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
+import 'dotenv/config';
+import express from 'express';
+import expressLayout from 'express-ejs-layouts';
+import db from './server/config/db.js';
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
+const [rows] = await db.query("SELECT * FROM user");
+
+console.log(rows);
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 
@@ -13,8 +17,8 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-app.use('/', require('./server/routes/main'));
+app.use('/', (await import('./server/routes/main.js')).default);
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}.`);
+    console.log(`Server app on port ${PORT}.`);
 });
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const router = express.Router();
 
 // Routes
@@ -16,4 +16,4 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
-module.exports = router;
+export default router;
