441647ca08419f6b9919fd41fee1a3f1b453d8f9
Ben Bonus <jbenbonus@gmail.com>
2024-10-05 18:57:36 -0700

    Kind of cooked on the layout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,21 +1,25 @@
 import React, {useState} from 'react';
 import './Header.css';
 
+import { Link } from 'react-router-dom';
+
 export default function Header() {
     return (
         <header className="header">
-            <a to="/" className="header_logo">BarterDB</a>
+            <div className="title">
+                <Link to="/" className="header_logo">BarterDB</Link>
+            </div>
 
             <nav className="nav">
                 <ul>
-                    <li key="home"><a href="/" className="nav-item">Home</a></li>
-                    <li key="about"><a href="/about" className="nav-item">About</a></li>
-                    <li key="contact"><a href="/contact" className="nav-item">Contact</a></li>
+                    <li key="home"><Link to="/" className="nav-item">Home</Link></li>
+                    <li key="about"><Link to="/about" className="nav-item">About</Link></li>
+                    <li key="contact"><Link to="/contact" className="nav-item">Contact</Link></li>
                 </ul>
             </nav>
 
-            <div className="header_button">
-                <button className="button">Sign In</button>
+            <div className="header_buttons">
+                <button className="header_button">Sign In</button>
             </div>
         </header>
     );
